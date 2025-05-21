8571362735fd7711b510193097f8a458f87686a3
Povilas <povilas.dailyda@gmail.com>
2025-05-21 23:23:17 +0300

    up

--- js/add-todo.js
+++ js/add-todo.js
@@ -22,5 +22,28 @@ formDOM.addEventListener('submit', event=>{// reikia rasyti 'submit' nes toks my
     // inputDOM.value // reikalingas kad irasys i inputa kazka jis sugeneruotu ta teksta ir dar skaiciu kelinta eilute. 
 });
 
+const formzDOM=document.querySelectorAll('form')
+const form1DOM=formzDOM[1];
+const input1DOM=document.querySelector('.input1');
+const btn1DOM=document.querySelector('.button1');
+const list1DOM=document.querySelector('.mano');
+ let laikrodis=1;
+form1DOM.addEventListener('submit',(event)=>{
+    event.preventDefault();
+
+    if(input1DOM.value.trim()===''){
+        return;
+    }
+    list1DOM.innerHTML+=`
+    <div class="todo-mano">
+            <p>${laikrodis}</p>
+            <p>${input1DOM.value}</p>
+        </div>`
+laikrodis++
+input1DOM.value='';
+});
+
+
+
 
 
--- js/todo.js
+++ js/todo.js
@@ -42,4 +42,42 @@ h1DOM.textContent=`Task planer (${count-deletedcount})`
 });
 
 
+const form1DOM=document.forms[1];
+const input1DOM=document.getElementById('task1');
+const btn1DOM=form1DOM.querySelector('button');
+const list1DOM=document.querySelector('.list1');
+const h11DOM=document.querySelectorAll('h1');
+const h12DOM=h11DOM[1];
+let laikrodis=0;
+let minus=0;
+form1DOM.addEventListener('submit',(event)=>{
+    event.preventDefault();
+   
+    list1DOM.innerHTML=`<div class="item1">
+                <div class="header1">
+                    <div class="index1">${laikrodis}</div>
+                    <button class="btn1" type="button">Delete</button>
+                </div>
+                <div class="content1">${input1DOM.value}</div>
+            </div> `+list1DOM.innerHTML;
+     input1DOM.value='';
+
+     
+laikrodis ++;
+input1DOM.focus();
+const deletebtnDOM=document.querySelectorAll('.btn1')
+
+for(const mygtukasDOM of deletebtnDOM){
+    mygtukasDOM.addEventListener('click',()=>{
+        mygtukasDOM.parentNode.parentNode.remove();
+
+        minus++
+        h12DOM.textContent=`Cia rezultatas ${laikrodis-minus}`
+    })
+    h12DOM.textContent=`Cia rezultatas ${laikrodis-minus}`
+}
+
+})
+
+
 
